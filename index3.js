d20179f8d69b25b988fde4ebae29789bff7c70fd
Saurabh Kumar <shikha@Saurabhs-MacBook-Air.local>
2023-11-02 23:28:02 +0530

    added morgan and custom middleware

--- index3.js
+++ index3.js
@@ -3,12 +3,25 @@ import express from "express";
 const app = express();
 const port = 3000;
 
+//custom middleware  --console.log HTTP request (GET,POST...)
+function logger(req, res, next){
+  console.log(req.method); //HTTP request
+  console.log(req.url);   //endpoint
+  next();
+}
 app.use(logger);
 
+
 app.get("/", (req, res) => {
-  res.send("Hello");
+  res.send("Hello World!");
 });
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
 });
+
+
+//bodyParser -> Preprocessing
+//morgan -> HTTP Request Logging
+//custom middleware -> Will try to make to do whatever we do
+
